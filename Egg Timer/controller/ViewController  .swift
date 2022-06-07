83d255a47163c0253305efcc29ef61394dcb7a28
andrepangilinan <andreonward@gmail.com>
2022-06-06 23:18:15 -0700

    update view controller

---
+++ Egg Timer/controller/ViewController  .swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    //Mark: - Outlets
+    @IBOutlet weak var TitleBarLbl: UILabel!
+    @IBOutlet weak var ProgressView: UIProgressView!
+    //Mark: - Dictionary and Array
+    //MARK: - Variabes and propertiest
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 19]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+//    let softTime = 5
+//    let mediumTime = 7
+//    let HardTime = 12
+//
+//Mark: - Action
+    @IBAction func softSeletedBtn(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+
+        ProgressView.progress = 0.0
+        secondsPassed = 0
+        TitleBarLbl.text = hardness
+//Mark: - timer code
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            ProgressView.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        } else {
+            timer.invalidate()
+            TitleBarLbl.text = "DONE!"
+
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+}
+}
+
+
